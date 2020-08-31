29007292738c3aba97c5f869762275581e31c08d
aaSuarez <aasuarez17pro@gmail.com>
2020-08-30 22:33:41 -0300

    subimos nuestro portafolio

---
+++ javascript/js.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector('.up');
+console.log(btn)
+window.onscroll = function (){
+    if(document.documentElement.scrollTop > 420){
+        btn.style.display = 'block';
+    }else{
+        btn.style.display = "none"
+    }
+}
+
+
+window.sr = ScrollReveal();
+sr.reveal('.contenido',{
+    duration:2000,
+    origin:'top'
+})
+sr.reveal('#head',{
+    duration:2000,
+    origin:'bottom'
+})
+sr.reveal('#lapiz',{
+    duration:2000,
+    origin:'bottom'
+})
+sr.reveal('#p-html',{
+    duration:2000,
+    origin:'bottom'
+})
+sr.reveal('#colores',{
+    duration:2000,
+    origin:'bottom'
+})
+
+
